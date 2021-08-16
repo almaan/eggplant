34abb53dcf68d2a93fb8110f149174dd2151e6eb
almaan <almaan@kth.se>
2021-08-16 10:54:08 +0200

    utils: creat_adata

--- test/utils.py
+++ test/utils.py
@@ -1,7 +1,11 @@
 import numpy as np
+import pandas as pd
+import anndata as ad
+import eggplant as eg
 from scipy.spatial.distance import cdist
 import torch as t
 import gpytorch as gp
+import utils as ut
 
 
 def create_model_input():
@@ -23,3 +27,28 @@ def create_model_input():
                 feature_values = t.tensor(values.astype(np.float32)),
                 meta = meta,
                 )
+
+def create_adata(pandas_landmark_distance = False):
+    model_input = create_model_input()
+    n_obs = model_input["domain"].shape[0]
+    var = pd.DataFrame(["gene1","gene2"],
+                       index = ["gene1","gene2"],
+                       columns= ["gene"],
+                       )
+
+    adata = ad.AnnData(np.random.random((n_obs,2)),
+                       var =var,
+                       )
+
+    adata.obsm["spatial"] = model_input["domain"].numpy()
+    lmks = model_input["landmark_distances"].numpy()
+    n_lmk = lmks.shape[1]
+    if pandas_landmark_distance:
+        lmks = pd.DataFrame(adata.obsm["landmark_distances"],
+                                                        columns = [f"L{k}" for k in range(n_lmk)],
+                                                        index = adata.obs.index,
+                                                        )
+    adata.obsm["landmark_distances"] = lmks
+    adata.layers["layer"] = adata.X.copy()
+    return adata
+
